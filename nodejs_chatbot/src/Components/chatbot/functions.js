a419ca00fa0afff3d73fecd51bd14edc109c76e6
Prabhmeet  Deol <deol@Prabhmeets-MacBook-Air.local>
2022-03-08 22:40:23 -0800

    added javascript app

--- nodejs_chatbot/src/Components/chatbot/functions.js
+++ nodejs_chatbot/src/Components/chatbot/functions.js
@@ -4,16 +4,15 @@ const questions = JSON.parse(require('../json_files/questions.json')); // we bas
 const responses =  JSON.parse(require('../json_files/responses.json')); 
 
 
-window.onload = function () {
+main_function = function (e) {
+	e.preventDefault(); // prevents the page from being reloaded.
 	var submit_button = document.getElementById('accept_response');
 	var resp = document.getElementById("user_response");
 	var system_response = document.getElementById("system_response");
 	submit_button.onclick = function (resp) {
 		// run this function, on click , and the input is recorded as resp.
+		system_response.innerHTML = "Button clicked.";
 	}
-
-
-
 
 }
 
